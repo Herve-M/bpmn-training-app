58f2c82cfc38927d940ae76c6933590e700869e1
MATYSIAK Herve <herve.matysiak@viacesi.fr>
2015-11-10 09:48:37 +0100

    First commit

---
+++ bpmn-pizza-app/src/main/java/pizzaapp/PizzaApp.java
@@ -0,0 +1,9 @@
+package pizzaapp;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Pizza App")
+public class PizzaApp extends ServletProcessApplication{
+
+}
