ea7be3c67774208b9fe8ee750855290ceb33ffdb
MATYSIAK Herve <herve.matysiak@viacesi.fr>
2015-11-12 11:13:26 +0100

    Add sendmail App

---
+++ bpmn-sendmail-app/src/main/java/sendmail/EmailFormBPMNApplication.java
@@ -0,0 +1,9 @@
+package sendmail;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Sendmail App")
+public class SendmailBPMNApplication extends ServletProcessApplication {
+
+}
