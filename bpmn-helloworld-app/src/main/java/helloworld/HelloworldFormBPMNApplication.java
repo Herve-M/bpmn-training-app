add411e2d939b6bcf3b61aa15f9b13f58df73cf4
MATYSIAK Herve <herve.matysiak@viacesi.fr>
2015-11-12 11:16:53 +0100

    Add Helloworld App

---
+++ bpmn-helloworld-app/src/main/java/helloworld/HelloworldFormBPMNApplication.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Helloworld App")
+public class HelloworldBPMNApplication extends ServletProcessApplication {
+
+}
