ceffe9e35e6fa28c8876eef5fc2e8bc1a30de0cb
MATYSIAK Herve <herve.matysiak@viacesi.fr>
2015-11-12 11:09:01 +0100

    Add TaskForm App

---
+++ bpmn-taskform-app/src/main/java/taskform/TaskFormBPMNApplication.java
@@ -0,0 +1,9 @@
+package taskform;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("TaskForm App")
+public class TaskFormBPMNApplication extends ServletProcessApplication {
+
+}
